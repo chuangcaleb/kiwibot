b45c4bde4e933798ad32ee9b7fa40ed001598723
chuangcaleb <balechuang@gmail.com>
2021-11-17 21:33:48 +0000

    Format

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
+from test import *
 from interface import *
 from canned_messages import *
 
 # Desc: Main entry point executable for docbot
 
+global NAME
+NAME = "Caleb"
+
 print(green("\n###################################################################################"))
 
 DOC_GREETING = [
-    "Hello, my name is DocBot! I am a chatbot assistant for computer programmers!",
-    "What's your name?"
+    "Hello, my name is DocBot! I am a chatbot assistant for computer programmers.",
+    "I know a lot about your favourite programming languages!",
+    "For example, ask me about the 'random' module in Python..."
+    # "What's your name?"
 ]
 
 docbot_says(DOC_GREETING)
 
-NAME = user_says()
+# NAME = user_says()
 
-NAME_RESPONSE = [f"Your name is: {NAME}"]
+# NAME_RESPONSE = [f"Your name is: {NAME}"]
 
-docbot_says(NAME_RESPONSE)
+# docbot_says(NAME_RESPONSE)
