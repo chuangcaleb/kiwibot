80841a31a4ea8250ca6749f22269770a60de14cf
chuangcaleb <balechuang@gmail.com>
2021-11-25 23:38:44 +0000

    Handle search errors: PageError, AttributeError, sent length

--- kiwibot/wikipedia_utils.py
+++ kiwibot/wikipedia_utils.py
@@ -1,13 +1,14 @@
 from nltk.tokenize import sent_tokenize
 import wikipedia
 import requests.exceptions
+import random
 
 
 def wikipedia_search(bot, search_query, is_random=False):
 
     try:
         responses = sent_tokenize(
-            wikipedia.summary(search_query, sentences=3, auto_suggest=is_random))
+            wikipedia.summary(search_query, sentences=3, auto_suggest=is_random))[:3]  # Limit to first three results, pages like "Jerry H. Bilik" pulls more sentences
         # TODO: ask for what next
         # responses.append("")
     except wikipedia.exceptions.DisambiguationError as e:
@@ -24,10 +25,11 @@ def wikipedia_search(bot, search_query, is_random=False):
 
 def wikipedia_random_search(bot):
 
-    # try:
-    responses = wikipedia_search(bot, wikipedia.random(), is_random=True)
-    # If random page gets disambiguated (so weird), try again
-    # except wikipedia.exceptions.DisambiguationError:
-    # wikipedia_random_search(bot)
+    try:
+        responses = wikipedia_search(bot, wikipedia.random(), is_random=True)
+    # If random page gets disambiguated (so weird), try random search again
+    except (wikipedia.exceptions.PageError, AttributeError):
+        print("Hold on...")
+        return wikipedia_random_search(bot)
 
     return responses
